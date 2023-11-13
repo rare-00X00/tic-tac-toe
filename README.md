8077842ff17828ed0e33b03debe3cc18d95aa4c9
rahul redd <rahulredd.ai2@yahoo.com>
2023-11-13 17:08:33 +0530

    [FIX] added and fixed functionality and symbol images

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# tic-tac-toe
+# Tic-Tac-Toe
+
+Exclusive tic-tac-toe app made using pure javascript along with html and css.
+
+You can play the game by visiting the GitHub page link provided, see 👉
+[tic-tac-toe](https://rare-00x00.github.io/tic-tac-toe/)
+
+_Note:_ Please note that this repository does not include the web socket logic
--- index.js
+++ index.js
@@ -2,40 +2,54 @@ var tiles = document.querySelectorAll('.container > :nth-child(2) > div');
 var player1 = {
     id: document.querySelector(".container > :nth-child(1)"),
     symbol: document.querySelector(".container > :nth-child(1) > :nth-child(1)"),
-    name: document.querySelector(".container > :nth-child(1) > :nth-child(2)"),
 }
 var player2 = {
     id: document.querySelector(".container > :nth-child(3)"),
     symbol: document.querySelector(".container > :nth-child(3) > :nth-child(1)"),
-    name: document.querySelector(".container > :nth-child(3) > :nth-child(2)"),
 }
 var disable = false;
 var symbol = "";
 var active_tiles = {};
+var fromKeyboardSpacebar = false;
 
-// Function used to get random symbol each initial loading
+function activePlayer() {
+    var randomNumber = Math.floor(Math.random() * 2);
+    if (randomNumber == 0) {
+        return "player1";
+    } else {
+        return "player2";
+    }
+}
+
 function randomSymbol() {
-    randomNumber = Math.floor(Math.random() * 2);
+    var non_active_symbol = "";
+    var randomNumber = Math.floor(Math.random() * 2);
     if (randomNumber == 0) {
         symbol = "o-symbol";
+        non_active_symbol = "x-symbol";
     } else {
         symbol = "x-symbol";
+        non_active_symbol = "o-symbol";
     }
+    return [symbol, non_active_symbol];
 }
 
-function addSymbolToPlayer() {
-    if (symbol === "o-symbol") {
-        player1.symbol.classList.add("player-symbol-o");
-        player2.symbol.classList.add("player-symbol-x");
+function randomization() {
+    var active_player = activePlayer();
+    var [active_symbol, non_active_symbol] = randomSymbol();
+
+    if (active_player === "player1") {
+        player1.id.classList.add("green");
+        player1.symbol.classList.add(active_symbol);
+        player2.symbol.classList.add(non_active_symbol);
     } else {
-        player1.symbol.classList.add("player-symbol-x");
-        player2.symbol.classList.add("player-symbol-o");
+        player2.id.classList.add("green");
+        player2.symbol.classList.add(active_symbol);
+        player1.symbol.classList.add(non_active_symbol);
     }
 }
 
-randomSymbol();
-addSymbolToPlayer();
-player1.id.classList.add("green");
+randomization();
 
 function changeSymbol() {
     if(symbol === "o-symbol") {
@@ -48,10 +62,18 @@ function changeSymbol() {
 }
 
 function blink3times(elements) {
-    debugger;
     id = setInterval(function() {
         for (var i=0; i<elements.length; i++) {
-            tiles[elements[i] - 1].classList.toggle("flickerbgimg");
+            tiles[elements[i] - 1].classList.toggle("flicker-bg-img");
+        }
+        if (player1.symbol.className === tiles[elements[0] - 1].classList[0]) {
+            player1.id.classList.toggle("flicker");
+            player1.id.classList.add("green");
+            player2.id.classList.remove("green");
+        } else {
+            player2.id.classList.toggle("flicker");
+            player2.id.classList.add("green");
+            player1.id.classList.remove("green");
         }
     }, 100);
     setTimeout(function() {
@@ -60,6 +82,7 @@ function blink3times(elements) {
 }
 
 function checkForWinner() {
+
     function patternMatch(combination) {
         if(active_tiles[combination[0]] === symbol &&
             active_tiles[combination[1]] === symbol &&
@@ -73,7 +96,7 @@ function checkForWinner() {
         return;
     } else {
         var flag = false;
-        const patterns = [[1,2,3], [1,4,7], [1,5,9], [4,5,6], [7,8,9], [3,5,7], [3,6,9]];
+        const patterns = [[1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,8], [3,6,9], [1,5,9], [3,5,7]];
         var matched_pattern = [];
         for (var i=0; i<patterns.length; i++) {
             if (patternMatch(patterns[i])) {
@@ -83,20 +106,64 @@ function checkForWinner() {
                 break;
             }
         }
-
-        if (flag === true) {
+        if (flag === true || Object.keys(active_tiles).length == 9) {
             blink3times(matched_pattern);
+            setTimeout(function() {
+                if (!fromKeyboardSpacebar) {
+                    reset();
+                }
+            }, 2000);
         }
+        debugger;
+    }
+}
+
+function tileEventHandler() {
+    if (disable != true) {
+        this.classList.add(symbol);
+        active_tiles[this.getAttribute('name')] = this.getAttribute('class');
+        checkForWinner();
+        changeSymbol();
     }
 }
 
 for(var i=0; i<tiles.length; i++) {
-    tiles[i].addEventListener('click', function() {
-        if (disable != true) {
-            this.classList.add(symbol);
-            active_tiles[this.getAttribute('name')] = this.getAttribute('class');
-            checkForWinner();
-            changeSymbol();
-        }
-    }, {'once': true});
+    tiles[i].addEventListener('click', tileEventHandler, {'once': true});
 }
+
+function reset() {
+
+    function removeClassesFromPlayer() {
+        player1.id.classList.remove("green");
+        player2.id.classList.remove("green");
+        player1.symbol.classList.remove("o-symbol");
+        player1.symbol.classList.remove("x-symbol");
+        player2.symbol.classList.remove("o-symbol");
+        player2.symbol.classList.remove("x-symbol");
+    }
+
+    function removeClasses(tile) {
+        tile.classList.remove("o-symbol");
+        tile.classList.remove("x-symbol");
+    }
+
+    disable = false;
+    active_tiles = {};
+    fromKeyboardSpacebar = false;
+    for(var i=0; i<tiles.length; i++) {
+        removeClasses(tiles[i]);
+        tiles[i].removeEventListener("click", tileEventHandler);
+        tiles[i].addEventListener("click", tileEventHandler, {'once': true});
+    }
+    removeClassesFromPlayer();
+    randomization();
+}
+
+document.addEventListener("keydown", function(event) {
+    if (event.key === " ") {
+        fromKeyboardSpacebar = true;
+        if (disable === true) {
+            reset();
+        }
+    }
+});
